22f2a7a024da59bf1bfd6ae29716198640257b1d
SHIRIN <efshitaria123@gmail.com>
2023-12-20 00:01:13 +0600

    Basic Router Setup

---
+++ main.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  QueryClient,
+  QueryClientProvider,
+
+} from "@tanstack/react-query";
+import {
+
+  RouterProvider
+} from "react-router-dom";
+import Routes from './Routes/Routes';
+import Provider from './Provider/Provider';
+
+
+const queryClient = new QueryClient();
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+
+    <QueryClientProvider client={queryClient}>
+      
+        <div >
+          <Provider><RouterProvider router={Routes} /></Provider>
+        </div>
+     
+    </QueryClientProvider>
+
+  </React.StrictMode>,
+)
